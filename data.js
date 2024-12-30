182165e62834e7f054a5a17869934b7f7d4a5316
carreiras <leonardo.turque10@gmail.com>
2024-12-30 11:30:39 -0300

    Create data.js

---
+++ data.js
@@ -0,0 +1,37 @@
+const players = [
+    {
+        name: "Lionel Messi",
+        age: 34,
+        position: "Atacante",
+        rating: 93,
+        club: "Paris Saint-Germain"
+    },
+    {
+        name: "Cristiano Ronaldo",
+        age: 36,
+        position: "Atacante",
+        rating: 91,
+        club: "Manchester United"
+    },
+    {
+        name: "Neymar Jr.",
+        age: 29,
+        position: "Atacante",
+        rating: 91,
+        club: "Paris Saint-Germain"
+    },
+    {
+        name: "Kevin De Bruyne",
+        age: 30,
+        position: "Meio-campista",
+        rating: 91,
+        club: "Manchester City"
+    },
+    {
+        name: "Kylian Mbappé",
+        age: 22,
+        position: "Atacante",
+        rating: 91,
+        club: "Paris Saint-Germain"
+    }
+];
